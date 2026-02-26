a7a788f0b5e7caf5d274ff747465c94e7312d785
noah1400 <noah.scholz.ns@gmail.com>
2026-02-26 12:02:47 +0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceHttps($this->app->isProduction());
+        if ($this->app->isProduction()) {
+            URL::forceHttps();
+            request()->server->set('HTTPS', 'on');
+        }
     }
 }
